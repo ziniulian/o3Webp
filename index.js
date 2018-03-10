9b4172ae74eef2725977c790e25b94aad19472f7
ziniulian <ziniulian@163.com>
2018-03-10 09:01:40 +0800

    测试

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ utNode.unpckBuffer = function (d/*as:Object*/, st/*as:Object*/, tag/*as:Object*/
 utNode.pckBuffer = function (d/*as:Object*/, salt/*as:Object*/, tag/*as:Object*/) {
 	var t = JSON.stringify(d);
 // console.log("pck : " + t.length);
-	tag.write(new Buffer(salt + (t.length) + "\r\n\r\n" + t));
+	tag.write(new Buffer(salt + "\r\n\r\n" + t));
 };
 
 // 逆 HTTP
@@ -136,7 +136,7 @@ var srv = new LZR.Node.Srv ({
 	port: process.env.OPENSHIFT_NODEJS_PORT || 80
 });
 
-var stres = "HTTP/1.1 200 OK\r\nAccept-Ranges: bytes\r\nContent-Length: ";
+var stres = "HTTP/1.1 200 OK\r\nAccept-Ranges: bytes";
 
 // 解析 post 参数
 srv.use("*", bodyParser.urlencoded({ extended: false }));
