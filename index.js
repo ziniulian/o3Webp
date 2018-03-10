c54f7d05e7c3d53da2ba0ac1d776c13995dc5af7
ziniulian <ziniulian@163.com>
2018-03-10 08:24:02 +0800

    测试

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ utNode.unpckBuffer = function (d/*as:Object*/, st/*as:Object*/, tag/*as:Object*/
 utNode.pckBuffer = function (d/*as:Object*/, salt/*as:Object*/, tag/*as:Object*/) {
 	var t = JSON.stringify(d);
 // console.log("pck : " + t.length);
-	tag.write(new Buffer(salt + (t.length) + "\r\n\r\n" + t));
+	tag.write(new Buffer(salt + (t.length + 10) + "\r\n\r\n" + t));
 };
 
 // 逆 HTTP
@@ -142,7 +142,6 @@ var stres = "HTTP/1.1 200 OK\r\nAccept-Ranges: bytes\r\nContent-Length: ";
 srv.use("*", bodyParser.urlencoded({ extended: false }));
 
 srv.ro.post("/ptth/", function (req, res) {
-console.log(11);
 	if (!utNode.ptth(req)) {
 		res.redirect(dmsrv.ds.main + "Err");
 	}
