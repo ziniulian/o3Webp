c781d49d21f45592304ab829c8ff49b4a515c125
ziniulian <ziniulian@163.com>
2018-11-04 13:06:19 +0800

    界面表格显示

--- web/js/PanMoni2.js
+++ web/js/PanMoni2.js
@@ -6,7 +6,8 @@ var dat = {
 	n: 1000000,	// 个数
 	p: 0,	// 指针
 	sv: [],		// 原始数据
-	c: [0],		// 结果集
+	bu: 12,		// 补刀位置
+	c: [[[], []]],		// 结果集
 
 	// 生成随机数
 	crtV: function () {
@@ -32,45 +33,62 @@ var dat = {
 	// 检查无重复号的间隔频率
 	check: function () {
 		var i, j, k, v, a = [];
-		// var min = dat.sv.length, b = [], bn = 0;	// 间隔信息（没啥号统计的，何种间隔都可能出现无间隔连续）
 		for (i = dat.p; i < dat.sv.length; i ++) {
 			v = dat.sv[i];
 			for (j = (i - 1); j >= dat.p; j --) {
 				if (dat.sv[j] === v) {
 					k = i - dat.p;
-// console.log(i + " , " + j + " , " + k);
 					if (dat.c[k]) {
 						dat.c[k] ++;
 					} else {
 						dat.c[k] = 1;
 					}
-					if (k > 18) {
-						dat.c[0] ++;
-					}
-
-					// // 间隔统计
-					// if (k > 20) {
-					// 	b.push({
-					// 		"p": dat.p,
-					// 		"e": i
-					// 	});
-					// 	if (bn) {
-					// 		bn = dat.p - bn;
-					// 		if (bn < min) {
-					// 			min = bn;
-					// 		}
-					// 	}
-					// 	bn = i;
-					// }
 
 					dat.p = i;
 					break;
 				}
 			}
 		}
-		console.log(dat.p + " / " + dat.sv.length);
-		console.log(dat.c);
-		// console.log(min);
-		// console.log(dat.sv);
+
+		dat.check1();
+	},
+
+	// 统计各关口的赢输比例
+	check1: function () {
+		var i, j, v, t, r, d;
+		tbDoe.innerHTML = "";
+		for (i = 1; i < dat.c.length; i ++) {
+			t = 0;
+			v = dat.c[i];
+			if (v) {
+				for (j = (i + 1); j < dat.c.length; j ++) {
+					if (dat.c[j]) {
+						t += dat.c[j];
+					}
+				}
+
+				// 页面更新
+				r = document.createElement("tr");
+				d = document.createElement("td");
+				d.innerHTML = i;
+				r.appendChild(d);
+				d = document.createElement("td");
+				d.innerHTML = v;		// 保关次数
+				r.appendChild(d);
+				d = document.createElement("td");
+				d.innerHTML = t;		// 破关次数
+				r.appendChild(d);
+				d = document.createElement("td");
+				d.innerHTML = dat.calPercent(v/(v + t));	// 保破比例
+				r.appendChild(d);
+				d = document.createElement("td");
+				d.innerHTML = (v * (36 - i)) - (t * i);		// 投保式盈利
+				r.appendChild(d);
+				d = document.createElement("td");
+				d.innerHTML = (t * (i - 1)) - (v * (37 - i));	// 投破式盈利
+				r.appendChild(d);
+				tbDoe.appendChild(r);
+			}
+		}
 	}
 };
